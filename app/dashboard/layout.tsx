c0ee3f18e333241b5dcff17c3f1437a4928d53e8
KIMLENDING <gotslaakaak@gmail.com>
2024-07-12 21:39:33 +0900

    배경색

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,11 +2,11 @@ import SideNav from "@/components/sideNav";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden ">
             <div className="w-full flex-none md:w-64">
                 <SideNav />
             </div>
-            <div className=" flex-grow p-6 md:overflow-y-auto md:p-12 bg-gradient-to-b from-background-start-rgb to-backgroundround-end-rgb ">{children}</div>
+            <div className=" flex-grow p-6 md:overflow-y-auto md:p-12 bg-gradient-to-t to-[rgb(80,80,80)] from-[rgb(16,16,16)]">{children}</div>
         </div>
     );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { basePath: "/Bg_templet_site", output: "export" };
-// basePath: "/Bg_templet_site", output: "export"
+// basePath: "/Bg_templet_site", output: "export" //배포할 때만 nextConfig에 추가
 export default nextConfig;
